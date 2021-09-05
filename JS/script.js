eca7a158aea27d049789b34aba653f7338087a3c
Berat Asrın CAFEROĞLU <beratasrin01@gmail.com>
2021-09-05 16:40:52 +0300

    kayıt olma sayfası tamamlandı, gerekli testler gerçekleştirilecek.

--- JS/script.js
+++ JS/script.js
@@ -1,45 +1,69 @@
 function postJSON(){
   // geeksforgeeks.org/how-to-send-a-json-object-to-a-server-using-javascript/
   // https://stackoverflow.com/questions/24468459/sending-a-json-to-server-and-retrieving-a-json-in-return-without-jquery
+ 
   let name = document.querySelector("#name").value;
   let taxNumber = document.querySelector("#tax_number").value;
   let mail = document.querySelector("#mail").value;
   let password = document.querySelector("#password").value;
-  let registerType = document.querySelector("input[name=register_type]:checked").value;
-  console.log(name,taxNumber,mail,password,registerType);
+  
+  if(name == ""){
+    alert("Organization Name is Missing.");
+  }
+  else if(taxNumber == ""){
+    alert("Organization Tax Number is Missing.");
+  }
+  else if(mail == ""){
+    alert("Organization E-Mail is Missing.");
+  }
+  else if(!mail.includes("@")){
+    alert("Enter a valid email address.")
+  }
+  else if(password == ""){
+    alert("Organization Password is Missing.");
+  }
+  else if(!(document.getElementById("producer").checked || document.getElementById("trader").checked)){
+    alert("Select Organization Type.")
+  }
+  else{
+    let registerType = document.querySelector("input[name=register_type]:checked").value;
+    console.log(name,taxNumber,mail,password,registerType);
+    // Create new xhr object
+    let xhr = new XMLHttpRequest();
+    let url = "http://localhost:8080/company/register";
 
-  // Create new xhr object
-  let xhr = new XMLHttpRequest();
-  let url = "http://localhost:8080/company/register";
+    xhr.onreadystatechange = function () {
+      console.log(xhr.readyState, xhr.status)
 
-  // open the connection
-  xhr.open("POST", url, true);
+      if (xhr.readyState == 4 && xhr.status == 200) {
 
-  // Set the request header i.e. which type of content you are sending
-  xhr.setRequestHeader("Content-Type", "application/json");
+        alert(xhr.responseText);
+        document.getElementById("register_form").reset();
+        console.clear();
 
-  /*
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
+      }
 
-        // Print received data from server
-        result.innerHTML = this.responseText;
+    };
 
-    }
-  };
-  */
- 
-  // Converting JSON data to string
-  let data = JSON.stringify({ 
-    "organizationName": name,
-    "taxNumber": taxNumber,
-    "mail": mail,
-    "registerType": registerType,
-    "password": password
-  });
+    // open the connection
+    xhr.open("POST", url, true);
 
-  // Sending data with the request
-  xhr.send(data);
+    // Set the request header i.e. which type of content you are sending
+    xhr.setRequestHeader("Content-Type", "application/json", "charset=UTF-8");
 
-  alert("Company Registered");
+    // Converting JSON data to string
+    let data = JSON.stringify({ 
+      "organizationName": name,
+      "taxNumber": taxNumber,
+      "mail": mail,
+      "registerType": registerType,
+      "password": password
+    });
+
+    // Sending data with the request
+    xhr.send(data);
+
+  }
+
+  
 }
