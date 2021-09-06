6baeb10efa09542cc50f63f4db140300a71ac3ec
Berat Asrın CAFEROĞLU <beratasrin01@gmail.com>
2021-09-07 02:32:23 +0300

    delete organization sayfası tasarlanıyor

--- JS/company_script.js
+++ JS/company_script.js
@@ -75,3 +75,104 @@ function registerNewCompany(){
   }
 
 }
+
+
+function searchToDelete(){
+  document.getElementById("delete_form_div").style.display = "none";
+  document.getElementById("delete_information_div").style.display = "flex";
+}
+
+function organizationInformationCancel(){
+  document.getElementById("delete_information_div").style.display = "none";
+  document.getElementById("delete_form_div").style.display = "flex";
+}
+
+function organizationInformationDelete(){
+  
+}
+
+/*
+var asideItems = [
+  {
+    text: "anasayfa",
+    value: "index.html",
+    onclick: "",
+  },
+  {
+    text: "hakkımızda",
+    value: "delete_company_page.html",
+    onclick: "",
+    subs: [
+      {
+        text: "hak 1",
+        value: "javascript:void(0)",
+        onclick: "organizationInformationCancel()",
+      },
+      {
+        text: "hak 2",
+        value: "javascript:void(0)",
+        onclick: "searchToDelete()",
+      },
+    ]
+  },
+  {
+    text: "kurumsal",
+    value: "javascript:void(0)",
+    onclick: "searchToDelete()",
+    subs: [],
+  },
+  {
+    text: "galeri",
+    value: "javascript:void(0)",
+    onclick: "searchToDelete()",
+  },
+]
+
+
+
+var asideUl = document.getElementById('sideNav');
+
+if(asideUl){
+  asideItems.map(function(v){
+
+    if(v.subs){
+      var subs = '';
+      v.subs.map(function(s){
+
+        subs += '<li><a href='+s.value+' onclick='+s.onclick+'>'+s.text+'</a></li>'
+      })
+
+      if(window.location.pathname.split("/").pop() == v.value){
+        asideUl.innerHTML += '<li><a href='+v.value+' onclick='+v.onclick+'>'+v.text+'</a><ul>'+subs+'</ul></li>';
+      }
+      else {
+        asideUl.innerHTML += '<li><a href='+v.value+' onclick='+v.onclick+'>'+v.text+'</a></li>';
+
+      }
+      
+
+    }
+    else {
+      asideUl.innerHTML += '<li><a href='+v.value+' onclick='+v.onclick+'>'+v.text+'</a></li>';
+
+    }
+  })
+}
+
+
+function delay(ms){
+  return new Promise(function(resolve,reject){
+    setTimeout(resolve, ms)
+  })
+}
+
+
+var test = async function(){
+  console.log('direk girdim');
+  await delay(5000);
+  console.log('1 sn sonra geldim')
+  await delay(5000);
+  console.log('1 sn sonra geldim')
+
+} 
+*/
